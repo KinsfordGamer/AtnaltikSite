57aff8fbb502dd8e05df6f759af1d5d5c123a704
KinsfordGamer <89835016+KinsfordGamer@users.noreply.github.com>
2026-05-20 14:54:37 +0500

    Delete backend directory

--- backend/app/api/v1/api.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from app.api.v1.endpoints import anime, stream, auth
-
-api_router = APIRouter()
-api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
-api_router.include_router(anime.router, prefix="/animes", tags=["animes"])
-api_router.include_router(stream.router, prefix="/stream", tags=["streaming"])
